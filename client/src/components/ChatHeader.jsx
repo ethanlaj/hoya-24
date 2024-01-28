4b7a46d81a6b4ea9c50510c6891078092f710d77
Ethan Lajeunesse <ethan@lajfam.com>
2024-01-27 20:44:19 -0500

    Change to Jay Bot

--- client/src/components/ChatHeader.jsx
+++ client/src/components/ChatHeader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ChatHeader = () => {
 	return (
 		<div className="bg-gray-800" style={{ height: "min-content" }}>
-			<div className="text-white text-lg text-center leading-10">Admissions AI Chat</div>
+			<div className="text-white text-lg text-center leading-10">Jay Bot</div>
 		</div>
 	);
 };
